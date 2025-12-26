05bbcc8cd960b5975c1854e5285613f2bda63290
TungstenX <MasterTungstenX@gmail.com>
2025-12-26 15:44:53 +0200

    Adding encoder switch logic, try 3.1

--- spi_app/SPIClient.py
+++ spi_app/SPIClient.py
@@ -102,6 +102,7 @@ class SPIClient:
 
         # Register interrupt callback
         self.reading = False
+        self.prev_CLK_state = 0
         self.cb = self.pi.callback(GPIO_TIRQ, pigpio.FALLING_EDGE, self.irq_callback)
         self.encoder = self.pi.callback(GPIO_RE_CLK, pigpio.RISING_EDGE, self.encoder_callback)
         self.encoder_sw = self.pi.callback(GPIO_RE_SW, pigpio.FALLING_EDGE, self.encoder_sw_callback)
